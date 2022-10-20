044280f16db36a9a684b722e8f0b220c058d2e7a
DavisSnow002 <nieveshernandezjesusdavid@gmail.com>
2022-10-19 22:22:27 -0500

    app routes

--- lib/Screens/screens.dart
+++ lib/Screens/screens.dart
@@ -1,3 +1,4 @@
 export 'package:practica_1/Screens/alert_screen.dart';
 export 'package:practica_1/Screens/homeScreen.dart';
 export 'package:practica_1/Screens/gameScreen.dart';
+export 'package:practica_1/Router/appRoutes.dart';
